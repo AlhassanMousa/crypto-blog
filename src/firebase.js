688e59b8ee4f0213ee54634db4cbec95dcb74967
Alhassan Mousa <42015026@hti.edu.eg>
2022-01-20 23:01:24 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,14 @@ import 'firebase/firestore';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAAN_MZ7IxyAylU1YLbso07pbJKSOIFX5g",
-  authDomain: "crypto-97a92.firebaseapp.com",
-  databaseURL: "https://crypto-97a92-default-rtdb.firebaseio.com",
-  projectId: "crypto-97a92",
-  storageBucket: "crypto-97a92.appspot.com",
-  messagingSenderId: "930079349896",
-  appId: "1:930079349896:web:e0f10ab7f19eac1ef4933d",
-  measurementId: "G-165WYMW98R",
+  apiKey: "##############",
+  authDomain: "################",
+  databaseURL: "################",
+  projectId:  "################",
+  storageBucket:  "################",
+  messagingSenderId:  "################",
+  appId:  "################",
+  measurementId:  "################",
 };
 
 
@@ -29,89 +29,3 @@ export const auth = firebase.auth();
 
 
 export default db;
-
-
-
-
-
-/*
-
-
-import * as firebase from 'firebase/compat/app';
-
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
-import 'firebase/firestore';
-
-
-import { initializeApp } from "firebase"
-
-const firebaseApp = initializeApp({ 
-
-  apiKey: "AIzaSyAAN_MZ7IxyAylU1YLbso07pbJKSOIFX5g",
-  authDomain: "crypto-97a92.firebaseapp.com",
-  databaseURL: "https://crypto-97a92-default-rtdb.firebaseio.com",
-  projectId: "crypto-97a92",
-  storageBucket: "crypto-97a92.appspot.com",
-  messagingSenderId: "930079349896",
-  appId: "1:930079349896:web:e0f10ab7f19eac1ef4933d",
-  measurementId: "G-165WYMW98R"
-});
-
-
-;
-const db = firebaseApp.firestore();
-
-export default db;
-************************************
-
-
-import { initializeApp } from 'firebase/app';
-import { getDatabase } from "firebase/database";
-
-import * as firebase from 'firebase/compat/app';
-
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
-import 'firebase/firestore';
-import { FiberManualRecord } from '@material-ui/icons';
-
-const firebaseConfig = {
- apiKey: "AIzaSyAAN_MZ7IxyAylU1YLbso07pbJKSOIFX5g",
-  authDomain: "crypto-97a92.firebaseapp.com",
-  databaseURL: "https://crypto-97a92-default-rtdb.firebaseio.com",
-  projectId: "crypto-97a92",
-  storageBucket: "crypto-97a92.appspot.com",
-  messagingSenderId: "930079349896",
-  appId: "1:930079349896:web:e0f10ab7f19eac1ef4933d",
-  measurementId: "G-165WYMW98R"
-};
-
-const app = initializeApp(firebaseConfig);
-
-const  database = getDatabase(app);
-
-export default database;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-*/
